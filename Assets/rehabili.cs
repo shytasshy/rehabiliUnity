37eef5f6c55a3fbf242d1b6081f1479b6f50546b
田島新 <tasshi-@tajimashin-no-MacBook-Pro.local>
2020-07-25 22:15:04 +0900

    Unityリハビリ

---
+++ Assets/rehabili.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rehabili : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int x = 1;
+        Debug.Log("test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/test/ImageMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageMove : MonoBehaviour
+{
+    Vector3 StartPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform tr = this.GetComponent<Transform>();
+        this.StartPos = tr.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform tr = this.GetComponent<Transform>();
+        Vector3 pos = tr.position;
+        pos.x = this.StartPos.x + Random.Range(-2.0f, 2.0f);
+        pos.y = this.StartPos.y + Random.Range(-2.0f, 2.0f);
+        tr.position = pos;
+    }
+}
